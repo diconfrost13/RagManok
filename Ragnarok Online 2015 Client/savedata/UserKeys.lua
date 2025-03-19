ca549383848999e0cc591537a0b05e6dd142159f
Diconfrost VaNz <diconfrost13@gmail.com>
2025-03-19 11:37:29 +0800

    Added emulator and 2015 client

---
+++ Ragnarok Online 2015 Client/savedata/UserKeys.lua
@@ -0,0 +1,7 @@
+USERKEY_1 = {
+	[9] = { EXE = "Hotkey 2-1", KEY1 = 81 },
+	[10] = { EXE = "Hotkey 2-2", KEY1 = 87 },
+	[11] = { EXE = "Hotkey 2-3", KEY1 = 69 },
+	[12] = { EXE = "Hotkey 2-4", KEY1 = 82 },
+}
+
